0be8b10ca422ad02efda3ca61c9d31c3295cefdb
william <willshinji@gmail.com>
2019-02-10 16:01:01 -0200

    set logger

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const morgan = require('morgan')
 
 const app = express()
+
+const accessLogStream = fs.createWriteStream(
+    path.join(`${__dirname}/logs`, 'access.log'), { flags: 'a' }
+)
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.listen(3000, () => console.log(`Server is running`))
 
